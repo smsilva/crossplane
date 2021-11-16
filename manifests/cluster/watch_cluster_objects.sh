09d323b793fb31c7774aad8530e5d528dedbb1ea
Silvio Silva <smsilva@gmail.com>
2021-11-16 11:14:51 -0300

    Creating a Object Cluster for Composition tests

---
+++ manifests/cluster/watch_cluster_objects.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+show_line() {
+  MESSAGE=${1}
+  OUTPUT=${2}
+
+  echo "${MESSAGE}:"
+  echo ""
+  (bash -c "${OUTPUT}" 2>&1) | awk '{ print "  " $0 }'
+  echo ""
+}
+
+show_line "Clusters (XRD)"                "kubectl get Cluster"
+show_line "Cluster Events"                "kubectl describe Cluster | grep 'Events:' -A 20"
+show_line "Crossplane Kubernetes Objects" "kubectl get Objects"
+show_line "Object Events"                 "kubectl describe Objects | grep 'Events:' -A 20"
+show_line "Secrets"                       "kubectl get Secrets -A | grep -vE '^kube|^local|^default.*-credentials|default-token|provider-|rbac|helm|crossplane-token'"
+show_line "Configmaps"                    "kubectl get Configmaps | grep -vE '^kube'"
--- scripts/crossplane_cluster_configuration.sh
+++ scripts/crossplane_cluster_configuration.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-kubectl apply -f  manifests/cluster/composite-resource-definition.yaml
+kubectl apply -f  manifests/cluster/composite-resource-definition.yaml && sleep 2
 
-sleep 2
-
-kubectl apply -f  "manifests/cluster/composition-${OVERCLOUD_TYPE}.yaml"
+#kubectl apply -f  "manifests/cluster/composition-${OVERCLOUD_TYPE}.yaml"
