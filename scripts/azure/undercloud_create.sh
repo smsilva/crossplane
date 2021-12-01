056f3f6db93aa4f92d651c9c442a6e30102174c1
Silvio Silva <smsilva@gmail.com>
2021-12-01 18:07:47 -0300

    AKS Undercloud Creation

--- scripts/azure/undercloud_create.sh
+++ scripts/azure/undercloud_create.sh
@@ -4,3 +4,5 @@ STACK_IMAGE="silviosilva/azure-kubernetes-cluster:0.1.0"
 stackrun ${STACK_IMAGE} apply -auto-approve
 
 env DEBUG=0 stackrun ${STACK_IMAGE} output -raw kubeconfig > "${KUBECONFIG?}"
+
+chmod 0600 "${KUBECONFIG?}"
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 THIS_SCRIPT="${0}"
 SCRIPTS_DIRECTORY=$(dirname "${THIS_SCRIPT}")
+PATH="${SCRIPTS_DIRECTORY}:${PATH}"
+
 CONFIG_FILE="${SCRIPTS_DIRECTORY}/../config.yaml"
+KUBECONFIG="${HOME}/.kube/undercloud"
 
-export KUBECONFIG="${HOME}/.kube/undercloud"
-export PATH="${SCRIPTS_DIRECTORY}:${PATH}"
+# Export
+export KUBECONFIG
+export PATH
 
+# Retrieve Undercloud Information
 UNDERCLOUD_TYPE=$(yq e .undercloud.type "${CONFIG_FILE?}")
 UNDERCLOUD_NAME=$(yq e .undercloud.name "${CONFIG_FILE?}")
 
+# Undercloud Creation: Bootstrap Crossplane Cluster
 env \
   KUBECONFIG="${KUBECONFIG}" \
   UNDERCLOUD_NAME="${UNDERCLOUD_NAME}" \
   "${SCRIPTS_DIRECTORY?}/${UNDERCLOUD_TYPE?}/undercloud_create.sh"
 
+# Cloud Credentials Creation
 cloud_secrets_create.sh
 
+# Crossplane Installation
+# - providers:
+#   - azure
+#   - google
+#   - helm
+#   - kubernetes
 crossplane_install.sh
 
+# ArgoCD Install using Crossplane Helm Provider
 argocd_install.sh
