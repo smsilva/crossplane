75e962c56e9b1605ebba2a742405345299916c98
Silvio Silva <smsilva@gmail.com>
2022-01-29 19:03:43 -0300

    ArgoCD Customization

--- scripts/ssh/argocd_create_ssh_auth_secret.sh
+++ scripts/ssh/argocd_create_ssh_auth_secret.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-BASE64ENCODED_ID_RSA=$(cat ${HOME}/.ssh/id_rsa | base64 | tr -d "\n") && \
+URL_GIT_REPOSITORY="git@github.com:smsilva/argocd.git"
+BASE64ENCODED_URL_GIT_REPOSITORY=$(echo ${URL_GIT_REPOSITORY} | base64)
+BASE64ENCODED_ID_RSA=$(cat ${HOME}/.ssh/id_rsa | base64 | tr -d "\n")
+
 kubectl apply -f - <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: argocd
 ---
 apiVersion: v1
 kind: Secret
@@ -11,4 +19,18 @@ type: kubernetes.io/ssh-auth
 data:
   ssh-privatekey: |
     ${BASE64ENCODED_ID_RSA}
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: argocd-repo-github-smsilva-argocd-creds
+  namespace: argocd
+  annotations:
+    managed-by: argocd.argoproj.io
+  labels:
+    argocd.argoproj.io/secret-type: repo-creds
+type: Opaque
+data:
+  sshPrivateKey: ${BASE64ENCODED_ID_RSA}
+  url: ${BASE64ENCODED_URL_GIT_REPOSITORY}
 EOF
